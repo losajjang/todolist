d84a87cfe6b44d9f75542b56ced09c4247a5e38f
losajjang <losajjang@naver.com>
2022-09-10 19:16:18 +0900

    init: 프로젝트 세팅

---
+++ src/components/TodosList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+type Props = {};
+
+function TodosList({}: Props) {
+  return <TodoItem />;
+}
+
+export default TodosList;
---
+++ src/containers/TodosContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TodosList from '../components/TodosList';
+
+function TodosContainer() {
+  return <TodosList />;
+}
+
+export default TodosContainer;

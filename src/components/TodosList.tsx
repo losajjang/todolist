12a0073871f19d6edaa03d0e5217ee57e0a7d37e
losajjang <losajjang@naver.com>
2022-09-16 00:01:22 +0900

    feat: todoList UI 개발

--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import TodoItem from './TodoItem';
 
 type TodoListProps = {
@@ -6,8 +7,18 @@ type TodoListProps = {
   done: boolean;
 };
 
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px 48px 32px;
+  overflow-y: auto;
+`;
+
 function TodosList({done, onToggle}: TodoListProps) {
-  return <TodoItem done={done} onToggle={onToggle} />;
+  return (
+    <TodoListBlock>
+      <TodoItem done={done} onToggle={onToggle} />
+    </TodoListBlock>
+  );
 }
 
 export default TodosList;
